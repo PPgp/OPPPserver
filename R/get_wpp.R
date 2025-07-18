89d9d346597df6185a8a3f72129f4d48663c80d8
Hana Sevcikova <hanas@uw.edu>
2025-07-17 17:00:50 -0700

    aggregation is done on the fly

--- R/get_wpp.R
+++ R/get_wpp.R
@@ -66,20 +66,15 @@ get_wpp_regions <- function(sort = TRUE) {
 #'
 #' @param country Name of country.
 #' @param year Integer value specifying the year for which data should be extracted.
-#'      The default is 2024 for 1x1 data and 2025 for 5x5 data, determined by the argument \code{n}.
-#'      For 5x5 data, only years that are divisible by 5 are available. 
 #' @param n Size of age groups. It can be either 1 (default) or 5.
 #'
 #' @return \code{data.table} object with female and male population counts (in thousands) by age.
-#'      It contains columns \code{age}, \code{popF} (female), \code{popM} (male). By default 
+#'      It contains columns \code{age}, \code{popF} (female), \code{popM} (male). By default
 #'      one year age groups are used. If \code{n} is 5, the grouping is by five years of age.
 #'
-#' @details If \code{n} is 1 (default), the data is extracted either from the dataset \code{\link[wpp2024]{popAge1dt}} or
-#'      \code{\link[wpp2024]{popprojAge1dt}}, depending on the specified \code{year}. 
-#'      If \code{n} is 5, the dataset is created using either \code{\link[wpp2024]{popAge5dt}} or
-#'      \code{\link[wpp2024]{popprojAge5dt}}. In this case, if \code{year} is not divisible by 5, it is rounded 
-#'      to the nearest year that is a multiple of 5.
-#'      
+#' @details The data is extracted either from the dataset \code{\link[wpp2024]{popAge1dt}} or
+#'      \code{\link[wpp2024]{popprojAge1dt}}, depending on the specified \code{year}.
+#'      If \code{n} is 5, the dataset is aggregated into 5-year age groups.
 #' @export
 #'
 #' @examples
@@ -93,25 +88,25 @@ get_wpp_regions <- function(sort = TRUE) {
 #' spain_pop60 <- get_wpp_pop("Spain", year = 1960)
 #' lines(spain_pop60[, age], spain_pop60[, popF], col = "red", lty = 2)
 #' lines(spain_pop60[, age], spain_pop60[, popM], col = "blue", lty = 2)
-#' legend("bottomleft", 
+#' legend("bottomleft",
 #'     legend = c("female 2024", "male 2024", "female 1960", "male 1960"),
 #'     bty = "n", lty = c(1, 1, 2, 2), col = rep(c("red", "blue"), 2))
-#'       
-get_wpp_pop <- function(country, year = NULL, n = 1){
+#'
+get_wpp_pop <- function(country, year = 2024, n = 1){
     name <- NULL # to satisfy R check
     if(!n %in% c(1, 5)) stop("Argument n must be 1 or 5")
-    if(is.null(year))
-        year <- if(n == 1) 2024 else 2025
-    yr <- round(year / n) * n
-    if(yr != year) warning("Argument year rounded to ", yr, ".")
-    dsname <- paste0("popAge", n, "dt")
-    pop <- get_wpp(dsname) # load observed data
-    if(nrow(pop[year == yr]) == 0) {
-        dsname <- paste0("popprojAge", n, "dt")
-        pop <- get_wpp(dsname) # load projected data
-    }
+    yr <- year # need to rename because a collision with the column name "year"
+    pop <- get_wpp("popAge1dt") # load observed data
+    if(nrow(pop[year == yr]) == 0)
+        pop <- get_wpp("popprojAge1dt") # load projected data
     pop_res <- pop[name == country & as.integer(year) == yr, c("age", "popF", "popM"), with = FALSE]
-    if(nrow(pop_res) == 0) stop("Either ", country, " or year ", yr, " not available in the WPP dataset ", dsname, ".")
+    if(nrow(pop_res) == 0) stop("Either ", country, " or year ", yr, " not available in the WPP data.")
+    if(n == 5){
+        # aggregate to 5-year age groups
+        age5 <- get_wpp("age5categories")
+        pop_res <- merge(pop_res, age5[, list(age1, agecat, age5 = age)], by.x = "age", by.y = "age1", sort = FALSE)
+        pop_res <- pop_res[, .(popM = sum(popM), popF = sum(popF)), by = c("agecat", "age5")][, agecat := NULL]
+    }
     return(pop_res)
 }
 
